5091246f73baeb12064d83f951ba67ee16dd9b88
Vegirskiy_Lord <dmitrijrodikovbvb@gmail.com>
2025-03-10 20:04:01 +0300

    Добавьте файлы проекта.

---
+++ App_Test/Core/ConnectionCore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace App_Test.Core
+{
+    public static class ConnectionCore
+    {
+        public static Frame? CoreFrame { get; set; }
+    }
+}
